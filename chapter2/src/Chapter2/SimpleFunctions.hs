3dd8844c5b910265cb20568739886602d5059df5
herereadthis <herereadthis.github@gmail.com>
2022-01-19 23:55:04 -0500

    concatenation

--- chapter2/src/Chapter2/SimpleFunctions.hs
+++ chapter2/src/Chapter2/SimpleFunctions.hs
@@ -8,5 +8,20 @@ Prelude> :l src/Chapter2/SimpleFunctions.hs
 *Chapter2.SimpleFunctions> firstOrEmpty ["hello", "world"]
 -}
 
-firstOrEmpty :: [[Char]] -> [Char]
+firstOrEmpty :: [[Char]] ->Char]
 firstOrEmpty lst = if not (null lst) then head lst else "empty"
+
+{-
+concatenation function
+symbolic names such as +++ go betwee variables
+when concatenating 2 lists where the first list is empty and the 2nd is not,
+then just return 2nd list.
+otherwise, create a new list by grabbing the head of first list, then append 
+with the function recursively.
+add element to the start of a list with colon -> 1 : [2,3] = [1,2,3]
+e.g. [1,2] +++ [3,4] means add 1 to a list foo. The list foo is the tail of
+[1,2] +++ [3,4], etc. 1:(2:[3,4])
+-}
+lst1 +++ lst2 = if null lst1 {- check if first list is empty -}
+                then lst2 -- return second list if first list is empty
+                else (head lst1) : (tail lst1 +++ lst2)
---
+++ docs/ghci.md
@@ -0,0 +1,20 @@
+GHCI
+
+
+```bash
+# Execute Shell Command
+Prelude> :!
+# e.g., Where am i?
+Prelude :! pwd
+
+# cd to directory
+Prelude> :cd PATH
+
+# load file
+Prelude> :l src/foo.hs
+Prelude> :load src/foo.hs
+
+# quit
+Prelude> :q
+Prelude> :quit
+```
